7894444b98df5a58e57d6b72fa0692cc83832213
wekers <fernando@wekers.org>
2023-07-28 17:40:11 -0300

    BCrypt password

--- password-grant/spring-boot-3-1-0-noauth/src/main/java/com/devsuperior/demo/DemoApplication.java
+++ password-grant/spring-boot-3-1-0-noauth/src/main/java/com/devsuperior/demo/DemoApplication.java
@@ -6,8 +6,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
+	/* //teste do spring security
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+	*/
+
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	/* //teste do spring security
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("ENCODE = " + passwordEncoder.encode("123456"));
+		boolean result = passwordEncoder.matches("123456", "$2a$10$7t2BUsRE95b9RbLejb4kRO7KFEIi.oqAHJxKftPqNRPsUttNYHXHW");
+		System.out.println("Resultado = " + result);
+	}
+	*/
 }
--- password-grant/spring-boot-3-1-0-noauth/src/main/resources/import.sql
+++ password-grant/spring-boot-3-1-0-noauth/src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$7t2BUsRE95b9RbLejb4kRO7KFEIi.oqAHJxKftPqNRPsUttNYHXHW');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$7t2BUsRE95b9RbLejb4kRO7KFEIi.oqAHJxKftPqNRPsUttNYHXHW');
 
 INSERT INTO tb_product (name) VALUES ('TV');
 INSERT INTO tb_product (name) VALUES ('Computer');
